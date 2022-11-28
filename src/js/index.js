3bebb83c353dd61b826ed373f2ae9003849a6cce
Alexander <kaamosdao@gmail.com>
2022-11-28 18:52:15 +0300

    add appearing fixed header

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
 import '../scss/style.scss';
+
+const init = () => {
+  window.addEventListener('scroll', () => {
+    const distance = 450;
+    const fixedheader = document.querySelector('.header-fixed');
+    if (window.scrollY >= distance) {
+      fixedheader.classList.add('header-fixed--show');
+    } else {
+      fixedheader.classList.remove('header-fixed--show');
+    }
+  });
+};
+
+init();
