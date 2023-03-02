89c7859edb16365674a197e6da19a086746c38c5
kaamosdao <kaamosdao@gmail.com>
2023-03-02 17:50:07 +0300

    Refactor construct-tour: add SectionConstructTour jsx component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import SectionChooseTour from './SectionChooseTour';
+import SectionConstructTour from './SectionConstructTour';
 
 const Main = () => {
   return (
     <main className="main">
       <h1 className="visually-hidden">Выбери свой тур c YourTour</h1>
       <SectionChooseTour />
+      <SectionConstructTour/>
     </main>
   );
 };
