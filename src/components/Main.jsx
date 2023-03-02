79bc9e7c5142117b0c47269a717e9bdf0051f906
kaamosdao <kaamosdao@gmail.com>
2023-03-02 17:53:15 +0300

    Refactor feedback: add SectionFeedback jsx component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SectionChooseTour from './SectionChooseTour';
 import SectionConstructTour from './SectionConstructTour';
+import SectionFeedback from './SectionFeedback';
 
 const Main = () => {
   return (
@@ -8,6 +9,7 @@ const Main = () => {
       <h1 className="visually-hidden">Выбери свой тур c YourTour</h1>
       <SectionChooseTour />
       <SectionConstructTour/>
+      <SectionFeedback />
     </main>
   );
 };
