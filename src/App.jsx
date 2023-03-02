e05b459efbbf50c149e44a0548ae40f0f7f6a454
kaamosdao <kaamosdao@gmail.com>
2023-03-02 17:30:52 +0300

    Refactor main: add main jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import HeaderFixed from './components/HeaderFixed';
+import Main from './components/Main';
 
 const App = () => {
   return (
     <>
       <Header />
       <HeaderFixed />
+      <Main />
     </>
   );
 };
