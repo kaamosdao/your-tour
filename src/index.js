9f74233aa94dab9f39256de77b18b45fcc64ebaf
kaamosdao <kaamosdao@gmail.com>
2023-03-09 12:47:16 +0300

    Refactor index.js: remove commentaries

--- src/index.js
+++ src/index.js
@@ -3,16 +3,6 @@ import init from './init.jsx';
 import './scss/style.scss';
 
 const render = () => {
-  // window.addEventListener('scroll', () => {
-  //   const distance = 450;
-  //   const fixedheader = document.querySelector('.headerFixed');
-  //   if (window.scrollY >= distance) {
-  //     fixedheader.classList.add('headerFixedShow');
-  //   } else {
-  //     fixedheader.classList.remove('showHeaderFixed');
-  //   }
-  // });
-
   const root = ReactDOM.createRoot(document.querySelector('#root'));
   const App = init();
   root.render(App);
