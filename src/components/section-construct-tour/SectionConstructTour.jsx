0bec753a11d05cff2ab2cc777dc8981a9b2c03e1
kaamosdao <kaamosdao@gmail.com>
2023-03-15 15:44:13 +0300

    Refactor SectionConstructTour: fix linter errors

--- src/components/section-construct-tour/SectionConstructTour.jsx
+++ src/components/section-construct-tour/SectionConstructTour.jsx
@@ -4,29 +4,28 @@ import InputFields from './InputFields';
 import LicenseConfirm from './LicenseConfirm';
 import styles from './SectionConstructTour.module.scss';
 
-const SectionConstructTour = () => {
-  return (
-    <section className={styles.constructTour}>
-      <h2 className={styles.title}>Собери свой тур</h2>
-      <p className={styles.description}>
-        Идейные соображения высшего порядка,&nbsp;
-        <br />а также рамки и место обучения кадров
-      </p>
-      <form className={styles.form}>
-        <InputFields />
-        <AgeConfirm />
-        <LicenseConfirm />
-        <div className={styles.buttons}>
-          <button className={styles.buttonFind} type="submit">
-            Найти тур
-          </button>
-          <button className={styles.buttonClear} type="reset">
-            Сбросить
-          </button>
-        </div>
-      </form>
-    </section>
-  );
-};
+const SectionConstructTour = () => (
+  <section className={styles.constructTour}>
+    <h2 className={styles.title}>Собери свой тур</h2>
+    <p className={styles.description}>
+      Идейные соображения высшего порядка,&nbsp;
+      <br />
+      а также рамки и место обучения кадров
+    </p>
+    <form className={styles.form}>
+      <InputFields />
+      <AgeConfirm />
+      <LicenseConfirm />
+      <div className={styles.buttons}>
+        <button className={styles.buttonFind} type="submit">
+          Найти тур
+        </button>
+        <button className={styles.buttonClear} type="button">
+          Сбросить
+        </button>
+      </div>
+    </form>
+  </section>
+);
 
 export default SectionConstructTour;
