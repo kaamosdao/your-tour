861fd29f190897322db9f5265f19b0a45a299776
kaamosdao <kaamosdao@gmail.com>
2023-03-15 16:04:27 +0300

    Refactor HistoryCard: fix linter errors

--- src/components/section-histories/HistoryCard.jsx
+++ src/components/section-histories/HistoryCard.jsx
@@ -1,37 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import cn from 'classnames';
 import styles from './HistoryCard.module.scss';
 
-const List = ({ items }) => {
-  return (
-    <ul className={styles.historyCardList}>
-      {items.map((name, i) => (
-        <li className={styles.historyCardItem} key={i}>
-          {name}
-        </li>
-      ))}
-    </ul>
-  );
+const List = ({ items }) => (
+  <ul className={styles.historyCardList}>
+    {items.map((name) => (
+      <li className={styles.historyCardItem} key={name}>
+        {name}
+      </li>
+    ))}
+  </ul>
+);
+
+const HistoryCard = ({
+  title, name, text, list, socials,
+}) => (
+  <li className={styles.item}>
+    <div className={styles.socials}>
+      {socials.map((social) => <a className={styles.socialLink} key={social} href="/">{social}</a>)}
+    </div>
+    <a className={cn(styles.historyCard, styles[name])} href="/">
+      <h3 className={styles.historyCardTitle}>{title}</h3>
+      <p className={styles.historyCardText}>{text}</p>
+      {list && <List items={list} />}
+      <footer className={styles.historyCardFooter}>
+        <p className={cn(styles.historyCardButton, 'button-more')}>
+          Подробнее
+        </p>
+      </footer>
+    </a>
+  </li>
+);
+
+HistoryCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  list: PropTypes.arrayOf(PropTypes.string).isRequired,
+  socials: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
-const HistoryCard = ({ title, name, text, list, socials }) => {
-  return (
-    <li className={styles.item}>
-      <div className={styles.socials}>
-        {socials.map((name, i) => <a className={styles.socialLink} key={i} href="">{name}</a>)}
-      </div>
-      <a className={cn(styles.historyCard, styles[name])} href="#">
-        <h3 className={styles.historyCardTitle}>{title}</h3>
-        <p className={styles.historyCardText}>{text}</p>
-        {list && <List items={list} />}
-        <footer className={styles.historyCardFooter}>
-          <p className={cn(styles.historyCardButton, 'button-more')}>
-            Подробнее
-          </p>
-        </footer>
-      </a>
-    </li>
-  );
+List.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default HistoryCard;
