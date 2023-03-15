37f665d7529806f39b1d2a7613a63ad3649d403d
kaamosdao <kaamosdao@gmail.com>
2023-03-15 14:30:38 +0300

    Change linter: add rule, prop-types, fix linter errors

--- src/components/section-choose-tour/TourCard.jsx
+++ src/components/section-choose-tour/TourCard.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import cn from 'classnames';
 import styles from './TourCard.module.scss';
 
-const TourCard = (props) => {
-  const  { name, title, price } = props;
-  return (
-    <a className={cn(styles.tourCard, styles[name])} href="#">
-      <h3 className={styles.title}>{title}</h3>
-      <p className={styles.price}>{price}</p>
-      <p className={cn(styles.button, 'button-more')}>Подробнее</p>
-    </a>
-  );
+const TourCard = ({ name, title, price }) => (
+  <a className={cn(styles.tourCard, styles[name])} href="/tour">
+    <h3 className={styles.title}>{title}</h3>
+    <p className={styles.price}>{price}</p>
+    <p className={cn(styles.button, 'button-more')}>Подробнее</p>
+  </a>
+);
+
+TourCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
 };
 
 export default TourCard;
