7e212e52858fc5e4bd7bf1c2a10d2469b5093883
kaamosdao <kaamosdao@gmail.com>
2023-03-15 15:46:16 +0300

    Refactor FeedbackCard: fix linter errors

--- src/components/section-feedback/FeedbackCard.jsx
+++ src/components/section-feedback/FeedbackCard.jsx
@@ -1,38 +1,49 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './FeedbackCard.module.scss';
 
-const FeedbackCard = ({ name, user, tour, text }) => {
-  return (
-    <li className={styles.card}>
-      {text.map((paragraph, i) => <p key={i} className={styles.text}>{paragraph}</p>)}
-      <div className={styles.user}>
-        <p className={styles.name}>{name}</p>
-        <p className={styles.tour}>Тур: {tour}</p>
-        <picture className={styles.avatarPicture}>
-          <source
-            type="image/webp"
-            srcSet={`src/assets/img/avatars/${user}.webp`}
-            width="75"
-            height="75"
-          />
-          <source
-            type="image/jpeg"
-            srcSet={`src/assets/img/avatars/${user}.jpg`}
-            width="75"
-            height="75"
-          />
-          <img
-            className={styles.avatar}
-            type="image/jpeg"
-            src={`src/assets/img/avatars/${user}.jpg`}
-            alt="Аватар"
-            width="75"
-            height="75"
-          />
-        </picture>
-      </div>
-    </li>
-  );
+const FeedbackCard = ({
+  name, user, tour, text,
+}) => (
+  <li className={styles.card}>
+    {text.map((paragraph, i) => <p key={i} className={styles.text}>{paragraph}</p>)}
+    <div className={styles.user}>
+      <p className={styles.name}>{name}</p>
+      <p className={styles.tour}>
+        Тур:
+        {tour}
+      </p>
+      <picture className={styles.avatarPicture}>
+        <source
+          type="image/webp"
+          srcSet={`src/assets/img/avatars/${user}.webp`}
+          width="75"
+          height="75"
+        />
+        <source
+          type="image/jpeg"
+          srcSet={`src/assets/img/avatars/${user}.jpg`}
+          width="75"
+          height="75"
+        />
+        <img
+          className={styles.avatar}
+          type="image/jpeg"
+          src={`src/assets/img/avatars/${user}.jpg`}
+          alt="Аватар"
+          width="75"
+          height="75"
+        />
+      </picture>
+    </div>
+  </li>
+);
+
+FeedbackCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  user: PropTypes.string.isRequired,
+  tour: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
 };
 
 export default FeedbackCard;
