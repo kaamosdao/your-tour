e6e1690baede4f6a5923b1536ed00ee9fcb63831
kaamosdao <kaamosdao@gmail.com>
2023-03-15 17:21:24 +0300

    Refactor init: fix linter errors

--- src/init.jsx
+++ src/init.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import App from './App.jsx';
+import App from './App';
 import store from './slices/index.js';
 
-export default () => (
+const init = () => (
   <Provider store={store}>
     <App />
   </Provider>
 );
+
+export default init;
