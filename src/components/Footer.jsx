8dfa23b2421691d6d2b46780dec2d20ac841a440
kaamosdao <kaamosdao@gmail.com>
2023-03-15 13:53:10 +0300

    Change Footer: fix linter errors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,30 @@
 import React from 'react';
-import cn from 'classnames'
+import cn from 'classnames';
 import styles from './Footer.module.scss';
 
-const Footer = () => {
-  return (
-    <footer className={styles.footer}>
-      <div className={styles.container}>
-        <p className={styles.text}>Наши социальные сети</p>
-        <ul className={styles.socials}>
-          <li className={cn(styles.socialsItem, styles.socialsInstagram)}>
-            <a className={styles.socialsLink} href="#">
-              instagram
-            </a>
-          </li>
-          <li className={cn(styles.socialsItem, styles.socialsFacebook)}>
-            <a className={styles.socialsLink} href="#">
-              facebook
-            </a>
-          </li>
-          <li className={cn(styles.socialsItem, styles.socialsVkontakte)}>
-            <a className={styles.socialsLink} href="#">
-              vkontakte
-            </a>
-          </li>
-        </ul>
-      </div>
-    </footer>
-  );
-};
+const Footer = () => (
+  <footer className={styles.footer}>
+    <div className={styles.container}>
+      <p className={styles.text}>Наши социальные сети</p>
+      <ul className={styles.socials}>
+        <li className={cn(styles.socialsItem, styles.socialsInstagram)}>
+          <a className={styles.socialsLink} href="https://instagram.com/">
+            instagram
+          </a>
+        </li>
+        <li className={cn(styles.socialsItem, styles.socialsFacebook)}>
+          <a className={styles.socialsLink} href="https://facebook.com/">
+            facebook
+          </a>
+        </li>
+        <li className={cn(styles.socialsItem, styles.socialsVkontakte)}>
+          <a className={styles.socialsLink} href="https://vk.com/">
+            vkontakte
+          </a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+);
 
 export default Footer;
