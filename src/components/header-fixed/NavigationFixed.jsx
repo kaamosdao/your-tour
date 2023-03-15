212739e5ca815de7a6d1538ee2d19c3d492234bf
kaamosdao <kaamosdao@gmail.com>
2023-03-15 14:03:50 +0300

    Refactor NavigationFixed: fix linter errors

--- src/components/header-fixed/NavigationFixed.jsx
+++ src/components/header-fixed/NavigationFixed.jsx
@@ -1,53 +1,51 @@
 import React from 'react';
 import styles from './NavigationFixed.module.scss';
 
-const NavigationFixed = () => {
-  return (
-    <nav className={styles.navigationFixed}>
-      <a className={styles.logo} href="#">
-        <picture>
-          <source
-            media="(min-width: 1024px)"
-            srcSet="src/assets/img/svg-icons/yourtour-mobile-black.svg"
-            width="181"
-            height="32"
-          />
-          <img
-            src="src/assets/img/svg-icons/yourtour-mobile-black.svg"
-            alt="YourTour logo"
-            width="133"
-            height="22"
-            loading="lazy"
-          />
-        </picture>
-      </a>
-      <ul className={styles.links}>
-        <li className={styles.item}>
-          <a className={styles.link} href="#">
-            Туры
-          </a>
-        </li>
-        <li className={styles.item}>
-          <a className={styles.link} href="#">
-            Создать тур
-          </a>
-        </li>
-        <li className={styles.item}>
-          <a className={styles.link} href="#">
-            Отзывы
-          </a>
-        </li>
-        <li className={styles.item}>
-          <a className={styles.link} href="#">
-            Истории
-          </a>
-        </li>
-      </ul>
-      <a className={styles.phone} href="tel:89999999999">
-        +7 999 999 99 99
-      </a>
-    </nav>
-  );
-};
+const NavigationFixed = () => (
+  <nav className={styles.navigationFixed}>
+    <a className={styles.logo} href="https://your-tour-test-task.vercel.app/">
+      <picture>
+        <source
+          media="(min-width: 1024px)"
+          srcSet="src/assets/img/svg-icons/yourtour-mobile-black.svg"
+          width="181"
+          height="32"
+        />
+        <img
+          src="src/assets/img/svg-icons/yourtour-mobile-black.svg"
+          alt="YourTour logo"
+          width="133"
+          height="22"
+          loading="lazy"
+        />
+      </picture>
+    </a>
+    <ul className={styles.links}>
+      <li className={styles.item}>
+        <a className={styles.link} href="https://your-tour-test-task.vercel.app/">
+          Туры
+        </a>
+      </li>
+      <li className={styles.item}>
+        <a className={styles.link} href="https://your-tour-test-task.vercel.app/">
+          Создать тур
+        </a>
+      </li>
+      <li className={styles.item}>
+        <a className={styles.link} href="https://your-tour-test-task.vercel.app/">
+          Отзывы
+        </a>
+      </li>
+      <li className={styles.item}>
+        <a className={styles.link} href="https://your-tour-test-task.vercel.app/">
+          Истории
+        </a>
+      </li>
+    </ul>
+    <a className={styles.phone} href="tel:89999999999">
+      +7 999 999 99 99
+    </a>
+  </nav>
+);
 
 export default NavigationFixed;
